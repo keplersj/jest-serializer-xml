0afed4dce4def91ef41d837767ed4ad674777507
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-01 13:57:38 -0600

    test: create unit test

---
+++ __tests__/index.js
@@ -0,0 +1,11 @@
+const serializer = require("../");
+
+describe("jest-serializer-xml", () => {
+  it("serializes an XML document correctly", () => {
+    expect.addSnapshotSerializer(serializer);
+
+    const testDocument = `<document>test</document>`;
+
+    expect(testDocument).toMatchSnapshot();
+  });
+});
